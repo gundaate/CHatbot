4f1d3224045e7c165d4f69138ad0199b1a8eb507
SamMorganstern <60161774+SamMorganstern@users.noreply.github.com>
2020-03-20 11:28:59 -0200

    final bot with fooballbot and cricketbot

--- nerdbot0.1.py
+++ nerdbot0.1.py
@@ -6,6 +6,49 @@ import wikipedia
 from datetime import datetime
 import selenium.webdriver as webdriver
 from selenium.webdriver import *
+import webbrowser
+#"https://soccer.sportmonks.com/api/v2.0/livescores/now?api_token=7xgsB80QJW7rw43oHUppSmmweEglNkjKbfFqTCBukA9BFrcLBtlN5eFUyd3h"
+def footballmain():
+    heading = 'FOOTBALLBOT'
+    heading = heading.center(50)  # https://www.tutorialspoint.com/python/string_center.htm
+
+    print("\033[0;30;43m  \n" + heading)  # code copied from:- http://ozzmaker.com/add-colour-to-text-in-python/
+    grettings = ["Hi", "Hey", "Hello", "Hiya"]
+    print(random.choice(grettings))
+    time.sleep(1.5)
+    print(" My name is FOOTBALLBOT")
+    time.sleep(1.5)
+    while True:
+        print("PLEASE SELECT A NUMBER FROM BELOW")
+        time.sleep(2.5)
+
+        print('''
+                              1- Player information
+                              2 - Today's matches
+                              3 - Up coming matches
+                              4 - Exit
+                              ''')
+
+        user_input = int(input("Please select a number \n"))
+
+        if user_input == 1:
+            players_name = str(input("Please enter player of your choice's name \n")).upper()
+            player_info = wikipedia.summary(players_name)
+            print(player_info)
+
+        elif user_input == 2:
+
+            footballPage = webbrowser.open("https://www.livescore.com")
+
+            print(footballPage)
+
+        elif user_input == 3:
+            comingGames = webbrowser.open("https://www.livescore.com/soccer/2020-03-21/3 ")
+            print(comingGames)
+
+        elif user_input == 4:
+            print("THANK YOU VERY MUCH FOR USING FOOTBALLBOT ,I HOPE ALL MY INFORMATION WERE USEFUL.GOD BLESS YOU")
+            break
 
 #log in to cumoodel website
 def cumoodel(Username,Password):
@@ -223,19 +266,21 @@ def cricketbot():
 # Run the chatbot
 def your_choise():
     choice = str(input('\nhi there! \nplease type your option from following: \n1 for '
-                       'talking with Mr.Nerd\n2 for talking with cricketbot'
-                       ' \n3 for log in to cumoodel website '
-                       '\n4 for log in to facebook \n5 for live Weather Rreport \n::>'))
+                       'talking with Mr.Nerd\n2 for talking with cricketbot\n3 for talking with'
+                       ' footballbot\n4 for log in to cumoodel website '
+                       '\n5 for log in to facebook \n6 for live Weather Rreport \n::>'))
     if choice == '1':
         NerdBot()
-    elif choice == '3':
-        cumoodel(0,0)
     elif choice == '4':
-        facebook(0,0)
+        cumoodel(0,0)
     elif choice == '5':
+        facebook(0,0)
+    elif choice == '6':
         weatherRreport(0)
     elif choice == '2':
         cricketbot()
+    elif choice == '3':
+        footballmain()
 
 
 while True:
